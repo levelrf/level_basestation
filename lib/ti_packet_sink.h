80d345a626993c3d98eda03d07d4f59fca5bea18
Rob Rhinehart <robertrhinehart@gmail.com>
2012-08-15 13:55:50 -0700

    adding gnuradio work

---
+++ lib/ti_packet_sink.h
@@ -0,0 +1,71 @@
+#ifndef TI_PACKET_SYNC
+#define TI_PACKET_SYNC
+
+#include <gr_sync_block.h>
+#include <gr_msg_queue.h>
+
+class ti_packet_sink;
+
+typedef boost::shared_ptr<ti_packet_sink> ti_packet_sink_sptr;
+
+ti_packet_sink_sptr make_ti_packet_sink (const std::vector<unsigned char>& sync_vector,
+			   gr_msg_queue_sptr target_queue);
+
+class ti_packet_sink : public gr_sync_block
+{
+private:
+ 	friend ti_packet_sink_sptr make_ti_packet_sink (const std::vector<unsigned char>& sync_vector,
+			   gr_msg_queue_sptr target_queue);	
+ 	
+ 	ti_packet_sink();   // private constructor	
+ 	
+ 	enum state_t {STATE_SYNC_SEARCH, STATE_HAVE_SYNC, STATE_HAVE_HEADER};	
+ 	
+ 	static const int MSG_LEN_POS = 8+1;        			  	// 8 byte sos header, 1 byte AM type
+ 	static const int MAX_PKT_LEN = 128 - MSG_LEN_POS - 1;	// remove header and CRC	
+ 	
+ 	gr_msg_queue_sptr  d_target_queue;					  	// where to send the packet when received
+ 	unsigned long long d_sync_vector;						// access code to locate start of packet
+ 	unsigned int	     d_threshold;						// how many bits may be wrong in sync vector
+ 	unsigned char      d_manchester;      				  	// do we use manchester encoding or not
+				  	
+ 	state_t            d_state;				  	
+				  	
+ 	unsigned long long d_shift_reg;						  	// used to look for sync_vector
+				  	
+ 	unsigned int       d_header;							// header bits
+ 	int		     	 d_headerbitlen_cnt;				  	// how many so far	
+ 	unsigned char      d_packet[MAX_PKT_LEN];				// assembled payload
+ 	unsigned char	     d_packet_byte;						// byte being assembled
+ 	unsigned char      d_packet_byte_manchester;  		  	// byte which is needed for manchester encoding
+ 	int		     	 d_packet_byte_index;				  	// which bit of d_packet_byte we're working on
+ 	int 		     	 d_packetlen;						// length of packet
+ 	int		     	 d_packetlen_cnt;					  	// how many so far
+ 	int		     	 d_payload_cnt;						  	// how many bytes in payload
+  
+protected:
+ 	ti_packet_sink(const std::vector<unsigned char>& sync_vector, 
+		       gr_msg_queue_sptr target_queue,
+		       int threshold);
+ 	
+ 	void enter_search();
+ 	void enter_have_sync();
+ 	void enter_have_header(int payload_len);
+
+ public:
+ 	~ti_packet_sink ();  // public destructor	
+ 	
+ 	// Where all the action really happens	
+ 	int work (int noutput_items,
+ 	                  gr_vector_int &ninput_items,
+ 	                  gr_vector_const_void_star &input_items,
+ 	                  gr_vector_void_star &output_items);
+ 	
+ 	// return true if we detect carrier
+ 	bool carrier_sensed() const
+ 	{
+ 	  return d_state != STATE_SYNC_SEARCH;
+ 	}
+};
+
+#endif /* TI_PACKET_SYNC */
---
+++ python/cc1100.py
@@ -0,0 +1,45 @@
+from gnuradio import gr
+from math import pi
+
+class demod(gr.hier_block):
+    def __init__(self, fg, sps = 8, symbol_rate = 38400, p_size = 13):
+        """
+        Hierarchical block for FSK demodulation.
+    
+        The input is the complex modulated signal at baseband
+        and the output is a stream of bytes.
+        """
+        
+        # Demodulate FM
+        sensitivity = (pi / 2) / sps
+        self.fmdemod = gr.quadrature_demod_cf(1 / sensitivity)
+
+        # Low pass the output of fmdemod to allow us to remove
+        # the DC offset resulting from frequency offset
+
+        alpha = 0.0512/sps
+        self.freq_offset = gr.single_pole_iir_filter_ff(alpha)
+        self.sub = gr.sub_ff()
+        
+        fg.connect(self.fmdemod, (self.sub, 0))
+        fg.connect(self.fmdemod, self.freq_offset, (self.sub, 1))
+        
+        # recover the clock
+        omega = sps
+        gain_mu = 0.03
+        mu = 0.5
+        omega_relative_limit = 0.0002
+        freq_error = 0.0
+        
+        gain_omega = .25*gain_mu*gain_mu        # critically damped
+        self.clock_recovery = gr.clock_recovery_mm_ff(omega, gain_omega, mu, gain_mu,
+                                                      omega_relative_limit)
+        
+        # Connect
+        fg.connect(self.sub, self.clock_recovery)
+        
+        #filesink = gr.file_sink(gr.sizeof_float, 'rx_fsk_test.dat')
+        #fg.connect(self.clock_recovery, filesink)
+        
+        # Initialize base class
+        gr.hier_block.__init__(self, fg, self.fmdemod, self.clock_recovery)
